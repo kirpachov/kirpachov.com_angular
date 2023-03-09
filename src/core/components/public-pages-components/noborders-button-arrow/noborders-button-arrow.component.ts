f933144ea1c0594b3585dab30d8fe3faaaffb77b
Sasha - Oleksandr Kirpachov <o.kirpachov@opinioni.net>
2023-03-09 19:07:15 +0100

    NEW noborders button arrow updated

--- src/core/components/public-pages-components/noborders-button-arrow/noborders-button-arrow.component.ts
+++ src/core/components/public-pages-components/noborders-button-arrow/noborders-button-arrow.component.ts
@@ -10,9 +10,21 @@ export class NobordersButtonArrowComponent {
 
   @Input() content?: TemplateRef<any> | string;
 
-  @Input() showArrowBefore: boolean = true;
+  @Input() showArrowBefore: boolean = false;
 
-  @Input() showArrowAfter: boolean = true;
+  @Input() showArrowAfter: boolean = false;
+
+  private _arrowPosition: 'before' | 'after' = 'after';
+  @Input() set arrowPosition(value: 'before' | 'after') {
+    this.showArrowAfter = value === 'after';
+    this.showArrowBefore = value === 'before';
+
+    this._arrowPosition = value;
+  }
+
+  get arrowPosition(): 'before' | 'after' {
+    return this._arrowPosition;
+  }
 
   get contentTemplate(): TemplateRef<any> | undefined {
     return this.content && this.content instanceof TemplateRef<any> ? this.content : undefined;
