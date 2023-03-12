8273522ab8a024aaed1936b1400bc350e6ecffba
Sasha - Oleksandr Kirpachov <o.kirpachov@opinioni.net>
2023-03-12 11:22:04 +0100

    NEW service section

--- src/core/components/home-page1/components/service-section/service-section.component.ts
+++ src/core/components/home-page1/components/service-section/service-section.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./service-section.component.scss']
 })
 export class ServiceSectionComponent {
+  readonly steps = Array.from({ length: 5 }).map((el, index) => ({
+    title: `Step ${index + 1}`,
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquet nisl, eget aliquam nisl lorem quis nisl.`
+  }));
 
+  get isOdd(): boolean {
+    return this.steps.length % 2 !== 0;
+  }
+
+  indexize(index: number): string {
+    return (index + 1).toString().padStart(2, '0');
+  }
 }
