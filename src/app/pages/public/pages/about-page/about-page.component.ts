38d8ce135f74220ed9af265d2f6e9424668720f9
Sasha - Oleksandr Kirpachov <o.kirpachov@opinioni.net>
2023-03-26 00:53:54 +0100

    NEW links to reference each section

--- src/app/pages/public/pages/about-page/about-page.component.ts
+++ src/app/pages/public/pages/about-page/about-page.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { AutoDestroy } from '@core/lib/autodestroy';
 import { TuiScrollService } from '@taiga-ui/cdk';
+import { BehaviorSubject, Subject, take, takeLast, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-about-page',
@@ -8,14 +10,59 @@ import { TuiScrollService } from '@taiga-ui/cdk';
   styleUrls: ['./about-page.component.scss']
 })
 export class AboutPageComponent {
+
+  readonly queryParams$: BehaviorSubject<Record<any, any>> = new BehaviorSubject<Record<any, any>>({});
+  queryParams: Record<any, any> = {};
+
+  @AutoDestroy private readonly autodestroy$ = new Subject<void>();
+
   constructor(
+    @Inject(TuiScrollService) private tuiScrollService: TuiScrollService,
     private readonly scrollService: TuiScrollService,
     private readonly router: Router,
     private readonly route: ActivatedRoute,
   ) { }
 
   ngAfterViewInit(): void {
-    if (this.router.url.indexOf('#') == -1)
-      this.scrollService.scroll$(window, 0).subscribe();
+    setTimeout(() => {
+      this.queryParams$.pipe(takeUntil(this.autodestroy$)).subscribe(
+        (qParams) => {
+          this.queryParams = qParams;
+
+          if (qParams['section']) this.parseSection();
+        }
+      )
+
+      this.queryParams$.next(this.route.snapshot.queryParams);
+      this.route.queryParams.pipe(takeUntil(this.autodestroy$)).subscribe((params) => {
+        this.queryParams$.next(params);
+      });
+    }, 500);
+  }
+
+  saveSection(section: string) {
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { section },
+      queryParamsHandling: 'merge'
+    });
+  }
+
+  private parseSection(): void {
+    const section = this.queryParams['section'];
+    if (section) this.scrollToSection(section);
+  }
+
+  private scrollToSection(sectionName: string): void {
+    const section = document.querySelector(`#${sectionName}`);
+
+    const data = section?.getBoundingClientRect();
+
+    if (!(data && data.top)) return;
+
+    this.tuiScrollService.scroll$(window, window.scrollY + data.top).pipe(takeUntil(this.autodestroy$)).subscribe({
+      complete: () => { }
+    }
+    )
   }
 }
