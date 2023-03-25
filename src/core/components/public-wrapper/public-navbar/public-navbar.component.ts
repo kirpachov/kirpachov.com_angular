9da580706e5a313f648fb072fe569058f8a0c72a
Sasha - Oleksandr Kirpachov <o.kirpachov@opinioni.net>
2023-03-26 00:31:59 +0100

    FIX closing menu on navigate

--- src/core/components/public-wrapper/public-navbar/public-navbar.component.ts
+++ src/core/components/public-wrapper/public-navbar/public-navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Router } from '@angular/router';
+import { AutoDestroy } from '@core/lib/autodestroy';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-public-navbar',
@@ -7,6 +9,9 @@ import { Subject } from 'rxjs';
   styleUrls: ['./public-navbar.component.scss']
 })
 export class PublicNavbarComponent {
+
+  @AutoDestroy
+  private readonly destroy$ = new Subject<void>();
 
   private readonly isMenuOpenChange$ = new Subject<boolean>();
 
@@ -21,9 +26,19 @@ export class PublicNavbarComponent {
     this.isMenuOpenChange$.next(value);
   }
 
-  constructor() {
-    this.isMenuOpenChange$.subscribe((value) => {
+  constructor(
+    private readonly router: Router
+  ) {
+    this.isMenuOpenChange$.pipe(
+      takeUntil(this.destroy$)
+    ).subscribe((value) => {
       this.updateBodyClass();
+    });
+
+    this.router.events.pipe(
+      takeUntil(this.destroy$)
+    ).subscribe(() => {
+      this.isMenuOpen = false;
     });
   }
 
